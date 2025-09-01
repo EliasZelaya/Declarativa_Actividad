65050e72fc36cb95dfd46fa881427a8e294e7fcf
EliasZelaya <00146322@uca.edu.sv>
2025-09-01 16:50:42 -0600

    add: comments

--- Actividad_1_09/a.pl
+++ Actividad_1_09/a.pl
@@ -1,16 +1,21 @@
 :- ['bas.pl'].
 
+%es como una funcion de derecha a izquierda, a la izquierda agarra el valor de la base y luego los niega, posteriormente lo manda a no es estudiante y da true o false 
+% :- funciona con un solo si, basicamente solo si no se cumple lo imprime o algo asi
 no_es_estudiante(Nombre) :-
     \+ estudiante(Nombre).
 
+% en este caso solo se muestra el primer estudiante y luego de imprimirlo deja de ejecutarse ! indica el corte
 es_mayor(X) :- estudiante(X), !.
 
 mostrar_estudiante :-
     estudiante(X),
     write('Estudiante: '), write(X), nl,
-    fail.
+%Basicamente funciona como un loop, repite la instruccion hasta que ya falla, en este caso hasta que no encuentra un nombre
+    fail. 
 mostrar_estudiante.
 
+% solo si existe en la base imprime que si existe y si no existe pues que no existe
 verificar_estudiante :-
     write('Ingrese un nombre: '), read(X),
     ( estudiante(X)
